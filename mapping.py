f574a82736e97b9bde59cab4492d953f6e6522b8
Hogar Maldonado <Hogar Maldonado@DESKTOP-J643LCQ>
2021-05-13 19:05:42 +0200

    pushing Maping class and other files

---
+++ mapping.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+# title           :mapping.py
+# description     :Enter Description Here
+# author          :Juan Maldonado
+# date            :5/13/2021
+# version         :0.0
+# usage           :SEE README.md
+# python_version  :3.7.10
+# conda_version   :4.9.2
+# ========================================================================================================
