8ad0bca7b7bf6f8df33b27a8565b83cdd263196c
dfu5823 <71274065+dfu5823@users.noreply.github.com>
2021-06-17 13:01:05 -0700

    Update unit tests for mapping

--- test_mapping.py
+++ test_mapping.py
@@ -7,28 +7,42 @@ import mapping
 
 class TestMapping:
 
-    def test_add_coordinate(self):
+    def test_add_and_remove_coordinate(self):
         mapper = mapping.ScreenMap()
         print("Adding Coordinate:")
         mapper.add_widget("test", 1, 2)
         assert mapper.gui_map == {'test': {'x': 1, 'y': 2}}
+        assert mapper.get_map != {'test': {'x': 1, 'y': 2}}
+        #get map returns the address of a map object, which may not be what you want
+        mapper.add_widget("best", 1, 2)
+        assert mapper.gui_map == {'best': {'x': 1, 'y': 2}, 'test': {'x': 1, 'y': 2}}
+        assert mapper.locate_label("test") == ('test', {'x': 1, 'y': 2})
+        assert mapper.locate_label("best") == ('best', {'x': 1, 'y': 2})
+        assert mapper.locate_label("test") != ('best', {'x': 1, 'y': 2})
+        mapper.remove_widget("best")
+        assert mapper.gui_map == {'test': {'x': 1, 'y': 2}}
+        assert mapper.locate_label("best") == None
+        mapper.remove_widget("test")
+        assert mapper.gui_map == {}
 
     def test_key_exists(self):
         mapper = mapping.ScreenMap()
+        print("Checking if Key -> test exists:")
+        assert mapper.if_widget_exists("test") == False
+        print("Adding Coordinate:")
+        mapper.add_widget("best", 1, 2)
+        print("Checking if Key -> test exists:")
+        assert mapper.if_widget_exists("test") == False
         print("Adding Coordinate:")
         mapper.add_widget("test", 1, 2)
         print("Checking if Key -> test exists:")
         assert mapper.if_widget_exists("test") == True
-
-    def test_key_nonexists(self):
-        mapper = mapping.ScreenMap()
-        print("Checking if Key -> test exists:")
-        assert mapper.if_widget_exists("test") == False
 
     def test_update_coordinate(self):
         mapper = mapping.ScreenMap()
         print("Adding Coordinate:")
         mapper.add_widget("test", 1, 2)
         print("Updating Values of Key-> test:")
-        mapper.update_coordinate("test", 3, 4)
-        assert mapper.gui_map == {'test': {'x': 3, 'y': 4}}
+        mapper.update_coordinate("test", -3, -4)
+        assert mapper.gui_map == {'test': {'x': -3, 'y': -4}}
+
