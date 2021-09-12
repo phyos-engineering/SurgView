14cde929484e1d76f58d39911f2e58f0aec2d1c6
phyos-engineering <engineering@phyos.net>
2021-09-12 20:30:11 +0200

    adding prompt to check_azure_speech

--- check_azure_speech.py
+++ check_azure_speech.py
@@ -1,8 +1,10 @@
 import azure_speech
 import json
 
-
 mic = azure_speech.SpeechEngine()
+
+# Play with command SELECT X AND SEND TO Y
+print("SAY ACTIVATION WORD: WAKE UP")
 if mic.recognize_keyword():
     result = mic.recognize_intent()
     json_result = json.loads(result)
