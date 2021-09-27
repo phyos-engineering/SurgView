cf7a5551c431c0ec60f163eb7e1a678e37846994
phyos-engineering <engineering@phyos.net>
2021-09-27 15:27:19 +0200

    enabling baboop prompt

--- events.py
+++ events.py
@@ -1,4 +1,5 @@
-# !/usr/bin/env python
+#
+#!/usr/bin/env python
 # title           :events.py
 # description     :Enter Description Here
 # author          :Juan Maldonado
@@ -87,8 +88,7 @@ class EventHandler:
                 self.num_interface_mappings + 1,
                 get_time(),
                 self.interface_reader.gui_map.get_map(),
-            )
-        )
+            ))
         self.num_interface_mappings += 1
 
         # print(self.register_device())
@@ -104,12 +104,12 @@ class EventHandler:
         while keep_listening:
             print("Listening for Activation Word...")
             if self.speech_engine.recognize_keyword():
-                # playsound("prompt.mp3")
+                playsound("prompt.mp3")
                 response = self.speech_engine.recognize_intent()
                 json_payload = json.loads(response)
                 self.session_log.luis_ai_responses.append(
-                    LuisResponse(self.num_luis_requests + 1, get_time(), json_payload)
-                )
+                    LuisResponse(self.num_luis_requests + 1, get_time(),
+                                 json_payload))
                 self.num_luis_requests += 1
                 print(json_payload)
                 self.process_intent(json_payload)
@@ -179,8 +179,7 @@ class EventHandler:
                 self.num_interface_mappings + 1,
                 get_time(),
                 self.interface_reader.gui_map.get_map(),
-            )
-        )
+            ))
         self.num_interface_mappings += 1
 
     def check_diff(self):
@@ -208,8 +207,7 @@ class EventHandler:
                 self.num_interface_mappings,
                 get_time(),
                 label_worfklow,
-            )
-        )
+            ))
         self.num_mapped_workflows += 1
 
         self.workflow += self.source + self.destinations
@@ -228,7 +226,8 @@ class EventHandler:
                 to_int_label = utils.transform_to_int(entity["entity"])
                 source_label = utils.clean_string(to_int_label)
 
-                button = self.interface_reader.gui_map.locate_label(source_label)
+                button = self.interface_reader.gui_map.locate_label(
+                    source_label)
 
                 if button is not None:
                     self.source.append(button)
@@ -237,7 +236,8 @@ class EventHandler:
                 to_int_label = utils.transform_to_int(entity["entity"])
                 destination_label = utils.clean_string(to_int_label)
 
-                button = self.interface_reader.gui_map.locate_label(destination_label)
+                button = self.interface_reader.gui_map.locate_label(
+                    destination_label)
 
                 if button is not None:
                     self.destinations.append(button)
@@ -255,7 +255,8 @@ class EventHandler:
                 to_int_label = utils.transform_to_int(entity["entity"])
                 source_label = utils.clean_string(to_int_label)
 
-                button = self.interface_reader.gui_map.locate_label(source_label)
+                button = self.interface_reader.gui_map.locate_label(
+                    source_label)
 
                 if button is not None:
                     self.source.append(button)
@@ -280,16 +281,21 @@ class EventHandler:
         else:
             online_status = True
 
-        obj = {"serialNumber": self.device_serial_number, "online": online_status}
+        obj = {
+            "serialNumber": self.device_serial_number,
+            "online": online_status
+        }
         result = requests.patch(url, data=json.dumps(obj), headers=headers)
         logging.debug(result.headers)
 
     def shutdown(self):
         print("Shutting Down...")
-        json_log = json.dumps(self.session_log, cls=EnhancedJSONEncoder, indent=4)
+        json_log = json.dumps(self.session_log,
+                              cls=EnhancedJSONEncoder,
+                              indent=4)
         with open(
-            self.interface_reader.session_logger.log_path + "/session.json", "w"
-        ) as outfile:
+                self.interface_reader.session_logger.log_path +
+                "/session.json", "w") as outfile:
             outfile.write(json_log)
 
         print(json_log)
