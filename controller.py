ebb35f356a4f71950aa0ee8c8d218fff3fd9fdff
phyos-engineering <engineering@phyos.net>
2021-10-05 21:13:04 +0200

    fixing python code

--- controller.py
+++ controller.py
@@ -10,39 +10,46 @@
 # conda_version   :4.8.3
 # =================================================================================================================
 from serial import Serial
-
 """
 The SerialController class allows us to interface with the Arduino Leonardo board via serial communication.
 """
 
 
 class SerialController:
-	def __init__(self):
-		"""
+    def __init__(self):
+        """
 		Constructor. Assigns serial port USB0 as communication medium between Arduino Leonardo and Pi4.
 		"""
-		self.serial_controller = Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=1)
-		self.serial_controller.flush()
+        self.serial_controller = Serial(port='/dev/ttyUSB0',
+                                        baudrate=9600,
+                                        timeout=1)
+        self.serial_controller.flush()
 
-	def move_mouse(self, x_coordinate: int, y_coordinate: int):
-		"""
+    def move_mouse(self, x_coordinate: int, y_coordinate: int):
+        """
 		Sends command, in byte format, to Arduino Leonardo to move mouse.
 		:param x_coordinate: X screen coordinate of target widget.
 		:param y_coordinate: Y screen coordinate of target widget.
 		"""
-		mouse_command = "{} {} {}\n".format("mouse", x_coordinate, y_coordinate)
-		self.serial_controller.write(bytes(mouse_command, 'utf-8'))
+        mouse_command = "{} {} {}\n".format("mouse", x_coordinate,
+                                            y_coordinate, 0)
+        self.serial_controller.write(bytes(mouse_command, 'utf-8'))
 
-		arduino_response = self.serial_controller.readline().decode('utf-8').rstrip()
-		print(arduino_response)
+        arduino_response = self.serial_controller.readline().decode(
+            'utf-8').rstrip()
+        print(arduino_response)
+        self.serial_controller.reset_input_buffer()
+        self.serial_controller.reset_output_buffer()
 
-	def type_with_keyboard(self, text_content: str):  # TO DO: Fix how Arduino parses string.
-		"""
+    def type_with_keyboard(
+            self, text_content: str):  # TO DO: Fix how Arduino parses string.
+        """
 		Sends command, in byte format, to Arduino Leonardo to type text.
 		:param text_content: Text intended to fill a text field.
 		"""
-		keyboard_command = "{} {}\n".format("keyboard", text_content)
-		self.serial_controller.write(bytes(keyboard_command, 'utf-8'))
+        keyboard_command = "{} {}\n".format("keyboard", text_content)
+        self.serial_controller.write(bytes(keyboard_command, 'utf-8'))
 
-		arduino_response = self.serial_controller.readline().decode('utf-8').rstrip()
-		print(arduino_response)
+        arduino_response = self.serial_controller.readline().decode(
+            'utf-8').rstrip()
+        print(arduino_response)
